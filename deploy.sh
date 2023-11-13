a0abe663c3ebd03bdb569de4b72d3c1e0b2d2df5
raurir <raurir@gmail.com>
2023-11-13 15:29:56 +1100

    move to archive

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-aws s3 sync ./ s3://www.esquemedia.com --exclude "*" --include "*.html" --include "*.png" --include "*.js" --include "site.webmanifest" --acl "public-read" # --dryrun
+aws s3 sync ./ s3://www.esquemedia.com/archive/2019/esquemedia --exclude "*" --include "*.html" --include "*.png" --include "*.js" --include "site.webmanifest" --acl "public-read" # --dryrun
